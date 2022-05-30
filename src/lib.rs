b6b646348f0b85cfc2e38f67210314edb87e8e10
julian <j.feinauer@pragmaticminds.de>
2022-05-30 19:45:25 +0200

    Refactoring of ChunkWriter into separate file.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ use std::io::Write;
 use compression::CompressionType;
 use encoding::{PlainInt32Encoder, TimeEncoder, TSEncoding};
 use statistics::{Statistics, StatisticsStruct};
+use crate::chunk_writer::{Chunkeable, ChunkMetadata, ChunkWriter};
 
 use crate::MetadataIndexNodeType::LeafDevice;
 use crate::murmur128::Murmur128;
@@ -22,6 +23,7 @@ mod test;
 mod utils;
 mod murmur128;
 mod schema;
+mod chunk_writer;
 
 const GET_MAX_DEGREE_OF_INDEX_NODE: usize = 256;
 const GET_BLOOM_FILTER_ERROR_RATE: f64 = 0.05;
@@ -33,7 +35,7 @@ const MAXIMAL_HASH_FUNCTION_SIZE: i32 = 8;
 const SEEDS: [u8; 8] = [5, 7, 11, 19, 31, 37, 43, 59];
 
 #[allow(dead_code)]
-enum IoTDBValue {
+pub enum IoTDBValue {
     DOUBLE(f64),
     FLOAT(f32),
     INT(i32),
@@ -187,146 +189,6 @@ impl PageWriter {
     }
 }
 
-trait Chunkeable {
-    fn write(&mut self, timestamp: i64, value: IoTDBValue) -> Result<(), &str>;
-
-    fn serialize(&mut self, file: &mut dyn PositionedWrite);
-
-    fn get_metadata(&self) -> ChunkMetadata;
-}
-
-struct ChunkWriter<T> {
-    data_type: TSDataType,
-    compression: CompressionType,
-    encoding: TSEncoding,
-    measurement_id: String,
-    current_page_writer: Option<PageWriter>,
-    offset_of_chunk_header: Option<u64>,
-    // Statistics
-    statistics: StatisticsStruct<T>,
-}
-
-impl Chunkeable for ChunkWriter<i32> {
-    fn write(&mut self, timestamp: i64, value: IoTDBValue) -> Result<(), &str> {
-        let value = match value {
-            IoTDBValue::INT(val) => val,
-            _ => {
-                return Err("wrong type!");
-            }
-        };
-        // Update statistics
-        self.statistics.update(timestamp, value);
-
-        match &mut self.current_page_writer {
-            None => {
-                // Create a page
-                self.current_page_writer = Some(PageWriter::new())
-            }
-            Some(_) => {
-                // do nothing
-            }
-        }
-        let page_writer = self.current_page_writer.as_mut().unwrap();
-        page_writer.write(timestamp, value)
-    }
-
-    fn serialize(&mut self, file: &mut dyn PositionedWrite) {
-        // Before we can write the header we have to serialize the current page
-        let buffer_size: u8 = match self.current_page_writer.as_mut() {
-            Some(page_writer) => {
-                page_writer.prepare_buffer();
-                page_writer.buffer.len()
-            }
-            None => 0,
-        } as u8;
-
-        let uncompressed_bytes = buffer_size;
-        // We have no compression!
-        let compressed_bytes = uncompressed_bytes;
-
-        let mut page_buffer: Vec<u8> = vec![];
-
-        // Uncompressed size
-        utils::write_var_u32(uncompressed_bytes as u32, &mut page_buffer);
-        // Compressed size
-        utils::write_var_u32(compressed_bytes as u32, &mut page_buffer);
-
-        // page data
-        match self.current_page_writer.as_mut() {
-            Some(page_writer) => {
-                page_buffer.write_all(&page_writer.buffer);
-            }
-            None => {
-                panic!("Unable to flush without page writer!")
-            }
-        }
-
-        // Chunk Header
-
-        // store offset for metadata
-        self.offset_of_chunk_header = Some(file.get_position());
-
-        file.write(&[5]).expect("write failed"); // Marker
-
-        write_str(file, self.measurement_id.as_str());
-        // Data Lenght
-        utils::write_var_u32(page_buffer.len() as u32, file);
-        // Data Type INT32 -> 1
-        file.write(&[self.data_type.serialize()])
-            .expect("write failed");
-        // Compression Type UNCOMPRESSED -> 0
-        file.write(&[self.compression.serialize()])
-            .expect("write failed");
-        // Encoding PLAIN -> 0
-        file.write(&[self.encoding.serialize()])
-            .expect("write failed");
-        // End Chunk Header
-
-        // Write the full page
-        file.write_all(&page_buffer);
-    }
-
-    fn get_metadata(&self) -> ChunkMetadata {
-        ChunkMetadata {
-            measurement_id: self.measurement_id.clone(),
-            data_type: self.data_type,
-            // FIXME add this
-            mask: 0,
-            offset_of_chunk_header: match self.offset_of_chunk_header {
-                None => {
-                    panic!("get_metadata called before offset is defined");
-                }
-                Some(offset) => offset,
-            } as i64,
-            statistics: self.statistics.clone(),
-        }
-    }
-}
-
-impl<'a, 'b, T> ChunkWriter<T> {
-    pub(crate) fn new(
-        measurement_id: String,
-        data_type: TSDataType,
-        compression: CompressionType,
-        encoding: TSEncoding,
-    ) -> Box<dyn Chunkeable> {
-        match data_type {
-            TSDataType::INT32 => {
-                let writer: ChunkWriter<i32> = ChunkWriter {
-                    data_type,
-                    compression,
-                    encoding,
-                    measurement_id: measurement_id.to_owned(),
-                    current_page_writer: None,
-                    offset_of_chunk_header: None,
-                    statistics: StatisticsStruct::<i32>::new(),
-                };
-                Box::new(writer)
-            }
-        }
-    }
-}
-
 struct GroupWriter {
     path: Path,
     chunk_writers: HashMap<String, Box<dyn Chunkeable>>,
@@ -370,43 +232,6 @@ impl GroupWriter {
                 .map(|(_, cw)| cw.get_metadata())
                 .collect(),
         )
-    }
-}
-
-#[derive(Clone)]
-struct ChunkMetadata {
-    measurement_id: String,
-    data_type: TSDataType,
-    mask: u8,
-    offset_of_chunk_header: i64,
-    // statistics: Box<dyn Statistics>,
-    statistics: StatisticsStruct<i32>,
-}
-
-impl Display for ChunkMetadata {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} (...)", self.measurement_id)
-    }
-}
-
-impl Serializable for ChunkMetadata {
-    fn serialize(&self, file: &mut dyn PositionedWrite) -> io::Result<()> {
-        file.write_all(&self.offset_of_chunk_header.to_be_bytes());
-        self.statistics.serialize(file)
-    }
-}
-
-impl ChunkMetadata {
-    fn serialize(
-        &self,
-        file: &mut dyn PositionedWrite,
-        serialize_statistics: bool,
-    ) -> io::Result<()> {
-        let result = file.write_all(&self.offset_of_chunk_header.to_be_bytes());
-        if serialize_statistics {
-            self.statistics.serialize(file);
-        }
-        return result;
     }
 }
 
